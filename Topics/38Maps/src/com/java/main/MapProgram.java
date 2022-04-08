b16d915b2632004b3ec67b09db03f3b59035008c
Auditor <leonel.s.auditor@accenture.com>
2022-04-08 13:35:14 +0800

    Updating topic project 38

--- Topics/38Maps/src/com/java/main/MapProgram.java
+++ Topics/38Maps/src/com/java/main/MapProgram.java
@@ -28,6 +28,23 @@ public class MapProgram {
 
 		System.out.println("================================================");
 
+//		languages.remove("Lisp");
+
+		if (languages.remove("Algol", "a family of algorithmic languages")) {
+			System.out.println("Algol removed");
+		} else {
+			System.out.println("Algol not removed, key/value pair not found");
+		}
+		
+		if (languages.replace("List", "This will not work", "a functional programming language with imperative features")) {
+			System.out.println("Lisp replaced");
+		} else {
+			System.out.println("List was not replaced");
+		}
+
+		System.out.println(languages.replace("Lisp", "a functional programming language with imperative features"));
+		System.out.println(languages.replace("Scala", "this will not be added"));
+
 		for (String key : languages.keySet()) {
 			System.out.println(key + " : " + languages.get(key));
 		}
