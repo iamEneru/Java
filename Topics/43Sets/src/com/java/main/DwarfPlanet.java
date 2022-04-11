21a273b3f1965d867b90555ec7d958cee6c6bc0f
Leonel Auditor <iamreianeru@gmail.com>
2022-04-12 00:18:34 +0800

    Adding topic projects 42 & 43

---
+++ Topics/43Sets/src/com/java/main/DwarfPlanet.java
@@ -0,0 +1,8 @@
+package com.java.main;
+
+public class DwarfPlanet extends HeavenlyBody {
+
+	public DwarfPlanet(String name, double orbitalPeriod) {
+		super(name, orbitalPeriod, BodyTypes.DWARF_PLANET);
+	}
+}
