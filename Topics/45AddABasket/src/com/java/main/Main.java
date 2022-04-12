f5afb3425875069aef7d465af49d1360bcf47781
Leonel Auditor <iamreianeru@gmail.com>
2022-04-12 23:44:59 +0800

    Updating topic project 45

--- Topics/45AddABasket/src/com/java/main/Main.java
+++ Topics/45AddABasket/src/com/java/main/Main.java
@@ -19,6 +19,9 @@ public class Main {
 		item = new StockItem("Cup", 0.50, 200);
 		stockList.addStock(item);
 
+		item = new StockItem("Cup", 0.45, 7);
+		stockList.addStock(item);
+
 		item = new StockItem("Door", 72.95, 4);
 		stockList.addStock(item);
 
@@ -39,6 +42,40 @@ public class Main {
 		for (String s : stockList.Items().keySet()) {
 			System.out.println(s);
 		}
+
+		Basket myBasket = new Basket("Customer");
+		sellItem(myBasket, "Car", 1);
+		System.out.println(myBasket);
+
+		sellItem(myBasket, "Car", 1);
+		System.out.println(myBasket);
+
+		sellItem(myBasket, "Car", 1);
+		sellItem(myBasket, "Spanner", 5);
+		System.out.println(myBasket);
+
+		sellItem(myBasket, "Juice", 4);
+		sellItem(myBasket, "Cup", 12);
+		sellItem(myBasket, "Bread", 1);
+
+		System.out.println(myBasket);
+
+		System.out.println(stockList);
 	}
 
+	public static int sellItem(Basket basket, String item, int quantity) {
+		StockItem stockItem = stockList.get(item);
+
+		if (stockItem == null) {
+			System.out.println("We don't sell " + item);
+			return 0;
+		}
+
+		if (stockList.sellStock(item, quantity) != 0) {
+			basket.addToBasket(stockItem, quantity);
+			return quantity;
+		}
+
+		return 0;
+	}
 }
