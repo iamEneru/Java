1bfb84177df1158e5a4d7d4dd044dcbb0a2f8168
Leonel Auditor <iamreianeru@gmail.com>
2022-04-14 16:15:30 +0800

    Adding JavaFX folder

---
+++ JavaFX/HelloWorldFX/src/application/Main.java
@@ -0,0 +1,23 @@
+package application;
+
+import javafx.application.Application;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		Parent root = new BorderPane();
+		Scene scene = new Scene(root, 400, 400);
+		scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+		primaryStage.setTitle("Hello World");
+		primaryStage.setScene(scene);
+		primaryStage.show();
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
