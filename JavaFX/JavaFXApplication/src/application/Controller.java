32cb5b238662a2265f79add0f7af183176603252
Leonel Auditor <iamreianeru@gmail.com>
2022-04-21 22:12:52 +0800

    Creating JavaFX project JavaFXApplication

---
+++ JavaFX/JavaFXApplication/src/application/Controller.java
@@ -0,0 +1,60 @@
+package application;
+
+import java.io.File;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.effect.DropShadow;
+import javafx.scene.layout.GridPane;
+import javafx.stage.DirectoryChooser;
+import javafx.stage.FileChooser;
+
+public class Controller {
+
+	@FXML
+	private Label label;
+
+	@FXML
+	private Button button4;
+
+	@FXML
+	private GridPane gridPane;
+
+	public void initialize() {
+		button4.setEffect(new DropShadow());
+	}
+
+	@FXML
+	public void handleMouseEnter() {
+		label.setScaleX(2.0);
+		label.setScaleY(2.0);
+	}
+
+	@FXML
+	public void handleMouseExit() {
+		label.setScaleX(1.0);
+		label.setScaleY(1.0);
+	}
+
+	@FXML
+	public void handleClick() {
+		FileChooser fileChooser = new FileChooser();
+		File file = fileChooser.showSaveDialog(gridPane.getScene().getWindow());
+		if (file != null) {
+			System.out.println(file.getPath());
+		}
+		
+		System.out.println("Chooser was cancelled");
+		
+		
+//		DirectoryChooser directoryChooser = new DirectoryChooser();
+//		File file = directoryChooser.showDialog(gridPane.getScene().getWindow());
+//		if (file != null) {
+//			System.out.println(file.getPath());
+//			return;
+//		}
+//
+//		System.out.println("Chooser was cancelled");
+	}
+}
