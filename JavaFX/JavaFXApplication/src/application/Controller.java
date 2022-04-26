ec7c2e3a228eedd424b7f8c848f2372204abd7b0
Leonel Auditor <iamreianeru@gmail.com>
2022-04-26 11:22:30 +0800

    Updating project

--- JavaFX/JavaFXApplication/src/application/Controller.java
+++ JavaFX/JavaFXApplication/src/application/Controller.java
@@ -1,9 +1,7 @@
 package application;
 
-import java.awt.Desktop;
 import java.io.File;
 import java.io.IOException;
-import java.net.URI;
 import java.net.URISyntaxException;
 import java.util.List;
 
@@ -12,6 +10,8 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.effect.DropShadow;
 import javafx.scene.layout.GridPane;
+import javafx.scene.web.WebEngine;
+import javafx.scene.web.WebView;
 import javafx.stage.FileChooser;
 import javafx.stage.FileChooser.ExtensionFilter;
 
@@ -25,6 +25,9 @@ public class Controller {
 
 	@FXML
 	private GridPane gridPane;
+
+	@FXML
+	private WebView webView;
 
 	public void initialize() {
 		button4.setEffect(new DropShadow());
@@ -68,7 +71,10 @@ public class Controller {
 	}
 
 	@FXML
-	public void handleLinkClick() throws IOException, URISyntaxException {
-		Desktop.getDesktop().browse(new URI("http://www.javafx.com"));
+	public void handleLinkClick() {
+//		Desktop.getDesktop().browse(new URI("http://www.javafx.com")); // throws IOException, URISyntaxException
+		WebEngine engine = webView.getEngine();
+		engine.load("http://www.javafx.com");
 	}
+
 }
